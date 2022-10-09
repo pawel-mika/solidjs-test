5d305394fc628cf5540dd59672b9dda8a2173753
Pawel Mika <pawel.mika@intive.com>
2022-10-09 18:35:46 +0200

    Small change - infinite scroll; broken styling yet

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -7,17 +7,33 @@ interface Photo {
 }
 
 const Gallery: Component = () => {
+    let container: HTMLDivElement;
     const [photos, setPhotos] = createSignal<Photo[]>([]);
+    const [page, setPage] = createSignal<number>(0);
+
+    const fetchPhotos = async (_page: string, _limit: string = '20') => {
+        const params = new URLSearchParams({_page, _limit});
+        const res = await fetch(`https://jsonplaceholder.typicode.com/photos?`  + params);
+        const respJson = await res.json();
+        const newPhotos = (respJson as Photo[])
+            .map((p) => ({ ...p, thumbnailUrl: `https://source.unsplash.com/random/320x240?q=${Number.parseInt(1000000 * Math.random())}` }));
+        setPhotos([...photos(), ...newPhotos]);
+    }
+
+    const handleScroll = (e: Event) => {
+        if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+            setPage(page() + 1)
+            fetchPhotos(page() as unknown as string);
+          }
+    }
 
     onMount(async () => {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=20`);
-        const respJson = await res.json();
-        setPhotos((respJson as Photo[]).map((p) => ({...p, thumbnailUrl: `https://source.unsplash.com/random/320x240?q=${Number.parseInt(1000000 * Math.random())}`})));
+        fetchPhotos(page() as unknown as string);
+        handleScroll(null);
     });
 
-    return (<>
+    return (<div class={styles.container} onScroll={handleScroll} ref={container}>
         <h2>Photo album</h2>
-
         <div class={styles.photos}>
             <For each={photos()} fallback={<p>Loading...</p>}>{(photo) =>
                 <figure class={styles.figure}>
@@ -26,7 +42,7 @@ const Gallery: Component = () => {
                 </figure>
             }</For>
         </div>
-    </>);
+    </div>);
 }
 
 export default Gallery;
