844e9703bbabb16c377721a86de0d77f1bdc3710
Pawel Mika <pawel.mika@intive.com>
2022-10-10 11:28:31 +0200

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { Component, createSignal, lazy } from 'solid-js';
+import { Component, createSignal, lazy, Show } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.scss';
 // import SelectMediaDevice from './SelectMediaDevices';
 // import Analyzer from './Analyzer';
-import Gallery from './Gallery';
+import Gallery, { Photo } from './Gallery';
 
-const Greeting = lazy(async () => {
-  // simulate delay
-  await new Promise(r => setTimeout(r, 2000))
-  return import("./Analyzer")
-});
-console.log(Greeting);
+// const Greeting = lazy(async () => {
+//   // simulate delay
+//   await new Promise(r => setTimeout(r, 2000))
+//   return import("./Analyzer")
+// });
+// console.log(Greeting);
 
 const App: Component = () => {
 
-  const [mediaDevice, setMediaDevice] = createSignal<MediaDeviceInfo>({} as MediaDeviceInfo);
+  const [isLoading, setLoading] = createSignal<boolean>(false);
+  const [photos, setPhotos] = createSignal<Photo[]>([]);
 
-  const onMediaDeviceSelected = (device: MediaDeviceInfo) => {
-    console.log(`from app: ${device}`);
-    setMediaDevice(device);
-  }
+  // const onMediaDeviceSelected = (device: MediaDeviceInfo) => {
+  //   console.log(`from app: ${device}`);
+  //   setMediaDevice(device);
+  // }
 
   return (
     <div class={styles.App}>
@@ -36,11 +37,14 @@ const App: Component = () => {
         </a>
       </header>
       <p class={styles.content}>
-        <Gallery></Gallery>
+        <Gallery isLoading={(loading) => setLoading(loading)} onPhotosChange={(photos)=>setPhotos(photos)}></Gallery>
         {/* <SelectMediaDevice onMediaDeviceSelected={onMediaDeviceSelected}></SelectMediaDevice> */}
         {/* <Analyzer mediaDevice={mediaDevice()}></Analyzer> */}
       </p>
-      <footer class={styles.footer}>e</footer>
+      <footer class={styles.footer}>
+        <Show when={isLoading()}><b>Loading...</b></Show>
+        Photos loaded: {photos().length}
+      </footer>
     </div>
   );
 };
--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,23 +1,37 @@
-import { Component, createSignal, For, onMount } from "solid-js";
+import { Component, createEffect, createSignal, For, mergeProps, onMount } from "solid-js";
 import styles from './Gallery.module.scss';
 
-interface Photo {
+export interface Photo {
     thumbnailUrl?: string;
     title?: string;
 }
 
-const Gallery: Component = () => {
+interface GalleryProperties {
+    onPhotosChange: (photos: Photo[]) => void;
+    isLoading: (loading: boolean) => void;
+}
+
+const Gallery: Component<GalleryProperties> = (props: GalleryProperties) => {
     let container: HTMLDivElement;
+    const [isLoading, setLoading] = createSignal<boolean>(false);
     const [photos, setPhotos] = createSignal<Photo[]>([]);
     const [page, setPage] = createSignal<number>(0);
 
+    props = mergeProps({onPhotosChange: () => {throw('unimplemented')}, isLoading: () => {throw('unimplemented')}}, props);
+
+    createEffect(() => props.onPhotosChange(photos()));
+
+    createEffect(() => props.isLoading(isLoading()));
+
     const fetchPhotos = async (_page: string, _limit: string = '32') => {
+        setLoading(true);
         const params = new URLSearchParams({_page, _limit});
         const res = await fetch(`https://jsonplaceholder.typicode.com/photos?`  + params);
         const respJson = await res.json();
         const newPhotos = (respJson as Photo[])
             .map((p) => ({ ...p, thumbnailUrl: `https://source.unsplash.com/random/320x240?q=${Number.parseInt(1000000 * Math.random())}` }));
         setPhotos([...photos(), ...newPhotos]);
+        setLoading(false);
     }
 
     const handleScroll = (e: Event) => {
