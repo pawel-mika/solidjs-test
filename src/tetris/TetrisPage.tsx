957e7254cb8c61b3eba94c8a75bc64a95bb55e80
Pawel Mika <pawel.mika@intive.com>
2022-11-25 17:21:27 +0100

    WIP Tetris remember high score

--- src/tetris/TetrisPage.tsx
+++ src/tetris/TetrisPage.tsx
@@ -4,69 +4,87 @@ import styles from './TetrisPage.module.scss';
 import createBoard, { Pixel, PixelType, Row } from "./Board";
 
 const TetrisPage: Component = () => {
-    const {width, height, screen, onKeyDown, pause, reset, gameState } = createBoard();
+  const getHiScore = () => sessionStorage.getItem('hiScore') || '0';
 
-    const renderPixel = (pixel: Pixel) => (<div classList={{
-        [styles.pixel]: true, 
-        [styles.p1]: pixel.type === PixelType.TAKEN,
-        [styles.p3]: pixel.type === PixelType.REMOVING}} 
-        style={pixel.style}>{pixel?.type}
-      </div>)
+  const { width, screen, onKeyDown, pause, reset, gameState } = createBoard();
 
-    const renderRow = (row: Row) => row.pixels.map((pixel) => renderPixel(pixel));
+  const [hiScore, setHiScore] = createSignal<string>(getHiScore());
 
-    onMount(() => {
-      document.addEventListener('keydown', onKeyDown);
-    });
+  const renderPixel = (pixel: Pixel) => (<div classList={{
+    [styles.pixel]: true,
+    [styles.p1]: pixel.type === PixelType.TAKEN,
+    [styles.p3]: pixel.type === PixelType.REMOVING
+  }}
+    style={pixel.style}>{pixel?.type}
+  </div>)
 
-    onCleanup(() => {
-      document.removeEventListener('keydown', onKeyDown);
-      pause();
-    });
+  const renderRow = (row: Row) => row.pixels.map((pixel) => renderPixel(pixel));
 
-    const newGame = () => {
-      reset();
-    }
-  
-    return (
-      <div class={appStyles.App}>
-        <header class={appStyles.header}>
-          WorkInProgress | level (speed): 0 | hiscore: 0
-        </header>
-        <div class={styles.content}>
-            <Show when={gameState().isGameOver}>
-              <div class={styles.gameOver}><span>Game Over</span></div>
-            </Show>
-            <Show when={gameState().isPaused}>
-              <div class={styles.paused}><span>Paused</span></div>
-            </Show>
-            <div class={styles.info}>
-              <p><b>Score: {gameState().score}</b></p>
-              <Show when={gameState().isGameOver}>
-                <button onclick={newGame}>New Game</button>
-              </Show>
-            </div>
-            <div class={styles.tetris} style={{
-              "grid-template-columns": `repeat(${width - 1}, 1fr) minmax(0, 1fr)`
-            }}>
-                {screen().map((row) => renderRow(row))}
-            </div>
-            <div class={styles.help}>
-              <p>
-                Movement: arrow keys - left/right/down
-              </p>
-              <p>
-                Rotate: arrow up
-              </p>
-              <p>
-                Pause: p
-              </p>
-            </div>
+  onMount(() => {
+    document.addEventListener('keydown', onKeyDown);
+  });
+
+  onCleanup(() => {
+    document.removeEventListener('keydown', onKeyDown);
+    pause();
+  });
+
+  const newGame = () => {
+    reset();
+  }
+
+  createEffect(() => {
+    const hiscore = Number.parseInt(sessionStorage.getItem('hiScore') || '0');
+    if (gameState().score > hiscore) {
+      sessionStorage.setItem('hiScore', `${gameState().score}`);
+      sessionStorage.setItem('hiScoreDate', `${new Date().getTime}`);
+      setHiScore(getHiScore());
+    };
+  });
+
+  return (
+    <div class={appStyles.App}>
+      <header class={appStyles.header}>
+        WorkInProgress | level (speed): 0
+      </header>
+      <div class={styles.content}>
+        <Show when={gameState().isGameOver}>
+          <div class={styles.gameOver}><span>Game Over</span></div>
+        </Show>
+        <Show when={gameState().isPaused}>
+          <div class={styles.paused}><span>Paused</span></div>
+        </Show>
+
+        <div class={styles.info}>
+          <p><b>Score: {gameState().score}</b></p>
+          <p>Hi Score: {hiScore()}</p>
+          <Show when={gameState().isGameOver}>
+            <button onclick={newGame}>New Game</button>
+          </Show>
         </div>
-        <footer class={appStyles.footer}>
-        </footer>
+
+        <div class={styles.tetris} style={{
+          "grid-template-columns": `repeat(${width - 1}, 1fr) minmax(0, 1fr)`
+        }}>
+          {screen().map((row) => renderRow(row))}
+        </div>
+
+        <div class={styles.help}>
+          <p>
+            Movement: arrow keys - left/right/down
+          </p>
+          <p>
+            Rotate: arrow up
+          </p>
+          <p>
+            Pause: p
+          </p>
+        </div>
       </div>
-    );
-  };
+      <footer class={appStyles.footer}>
+      </footer>
+    </div>
+  );
+};
 
 export default TetrisPage;
